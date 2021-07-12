7559258b8786b88d9d6c8504d5b83e3e7b74ae56
Tao He <sighingnow@gmail.com>
2021-07-13 00:53:05 +0800

    Update vcpkg configs, fixes #72.
    Resolves microsoft/vcpkg#18901 as well.
    
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- vcpkg/etcd-cpp-apiv3/portfile.cmake
+++ vcpkg/etcd-cpp-apiv3/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO etcd-cpp-apiv3/etcd-cpp-apiv3
-    REF v0.2.1
-    SHA512 2112d4f59386e7752f1e7930faf1604c0208dcb7ee8fc431e6cc4c5ce3b4616cd8dacfd68af560d0f973b1b8a49d881d33e29923ec3a4b16a4de1a4f5db270c5
+    REF 0dc89e4c1edace3c135506a4c704f39ef9195ae7
+    SHA512 11ce76f1191adef4e971ee7a36e64356a0f2e1a56760eefe7cf0de082c0f27f0b245df2dc0512f570af217083fad8ef6fd81c1182054333a3104a44786adb9a1
     HEAD_REF master
 )
 
@@ -20,6 +20,7 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/etcd-cpp-api)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
 
 vcpkg_copy_pdbs()
 
