c47f5af6f2224cce1484014581c041209b5a48c4
Tao He <linzhu.ht@alibaba-inc.com>
2023-12-20 16:18:25 +0800

    Enable ipv6 endpoints support
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

---
+++ tst/EtcdResolverTest.cpp
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <iostream>
+
+#include "etcd/Client.hpp"
+
+static const std::string etcd_v4_url =
+    etcdv3::detail::resolve_etcd_endpoints("http://127.0.0.1:2379");
+static const std::string etcd_v6_url =
+    etcdv3::detail::resolve_etcd_endpoints("http://::1:2379");
+
+TEST_CASE("test ipv4 connection") {
+  std::cout << "ipv4 endpoints: " << etcd_v4_url << std::endl;
+  etcd::Client etcd(etcd_v4_url);
+  REQUIRE(etcd.head().get().is_ok());
+}
+
+TEST_CASE("test ipv6 connection") {
+  std::cout << "ipv6 endpoints: " << etcd_v6_url << std::endl;
+  etcd::Client etcd(etcd_v6_url);
+  REQUIRE(etcd.head().get().is_ok());
+}
