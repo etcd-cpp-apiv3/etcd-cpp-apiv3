662573b6bf9f96f6f5dedcb4fa028b17081b6891
zhangxiaoyu.york <zhangxiaoyu.york@bytedance.com>
2024-11-12 10:13:53 +0800

    feature:support  ipv6 address (#280)
    Problem Summary:
    Now the etcd client address do not support host format such as
    http://[ipv6]:port.
    
    and [RFC
    3986](https://datatracker.ietf.org/doc/html/rfc3986#section-3.2.2)
    ```
    A host identified by an Internet Protocol literal address, version 6
    [[RFC3513](https://datatracker.ietf.org/doc/html/rfc3513)] or later, is distinguished by enclosing the IP literal
    within square brackets ("[" and "]").  This is the only place where
    square bracket characters are allowed in the URI syntax.  In
    anticipation of future, as-yet-undefined IP literal address formats,
    an implementation may use an optional version flag to indicate such a
    format explicitly rather than rely on heuristic determination.
    
          IP-literal = "[" ( IPv6address / IPvFuture  ) "]"
    ```
    the ipv6 address is distinguished by enclosing the IP literal within
    square brackets ("[" and "]").
    
    What is changed and how it works:
    if we find the host part of a URL starts with a "[" and ends with a "]".
    it will be specifically judged whether it is a valid IPv6 address. If it
    is a valid address, it can be returned directly without call the
    `getaddrinfo` interface.

--- tst/EtcdResolverTest.cpp
+++ tst/EtcdResolverTest.cpp
@@ -9,6 +9,9 @@ static const std::string etcd_v4_url =
     etcdv3::detail::resolve_etcd_endpoints("http://127.0.0.1:2379");
 static const std::string etcd_v6_url =
     etcdv3::detail::resolve_etcd_endpoints("http://::1:2379");
+// http://[ipv6]:port url
+static const std::string etcd_ipv6_url =
+    etcdv3::detail::resolve_etcd_endpoints("http://[::1]:2379");
 
 TEST_CASE("test ipv4 connection") {
   std::cout << "ipv4 endpoints: " << etcd_v4_url << std::endl;
@@ -20,4 +23,8 @@ TEST_CASE("test ipv6 connection") {
   std::cout << "ipv6 endpoints: " << etcd_v6_url << std::endl;
   etcd::Client etcd(etcd_v6_url);
   REQUIRE(etcd.head().get().is_ok());
+
+  std::cout << "ipv6 endpoints: " << etcd_ipv6_url << std::endl;
+  etcd::Client etcd1(etcd_ipv6_url);
+  REQUIRE(etcd1.head().get().is_ok());
 }
